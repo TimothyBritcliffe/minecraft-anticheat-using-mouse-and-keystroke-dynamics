c657cedcef9d33d369aeeb16a5755d2e2e84d5f5
Timothy Britcliffe <timothy@britcliffe.com>
2026-03-14 19:33:48 -0700

    Initial Commit

---
+++ keylogging.py
@@ -0,0 +1,8 @@
+#add the functions for key logging here. Keep it modular - using functions for everything (also give specific tasks separate functions)
+#
+#
+# Make sure to include a 'main-guard' that runs the main key_log() function to test the functionality
+#
+
+def key_log():
+    pass
---
+++ mouselogging.py
@@ -0,0 +1,8 @@
+#add teh functions for mouse logging here. Keep it modular - using functions for everything (also give specific tasks separate functions)
+# will also need a write_csv() function etc etc etc
+#
+# Make sure to include a 'main-guard' that runs the main mouse_log() function to test the functionality
+#
+
+def mouse_log():
+    pass
